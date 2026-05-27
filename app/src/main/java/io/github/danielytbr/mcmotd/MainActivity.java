6fd84bff68152f4de04112d6592b6d6ca5c6c8d2
DanielYTbr ツ <cd095959@gmail.com>
2026-05-27 09:38:26 -0300

    update app/src/main/java/io/github/danielytbr/mcmotd/MainActivity.java

--- app/src/main/java/io/github/danielytbr/mcmotd/MainActivity.java
+++ app/src/main/java/io/github/danielytbr/mcmotd/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends Activity {
             "1.18.2 (${R.string.protocol} 758)",
             "1.19.4 (${R.string.protocol} 762)",
             "1.20.4 (${R.string.protocol} 765)",
-            ${R.string.auto}
+            R.string.auto
         };
         
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, 
